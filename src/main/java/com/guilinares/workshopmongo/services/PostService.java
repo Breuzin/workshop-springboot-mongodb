834b896a546dafec3cde7919610d2850e5ca3c08
Breuzin <guimine.20@gmail.com>
2022-03-18 13:22:01 -0300

    Simple query with query methods

--- src/main/java/com/guilinares/workshopmongo/services/PostService.java
+++ src/main/java/com/guilinares/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.guilinares.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,4 +21,7 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Not Found object"));
 	}
 	
+	public List<Post> findByTitle(String text) {
+		return repository.findByTitleContainingIgnoreCase(text);
+	}
 }
